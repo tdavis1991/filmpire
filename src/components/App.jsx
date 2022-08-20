ec4e2ec2b0b4391cb9bee0d2774c28102f628fe2
Tevin_Davis <tdavis.070991@gmail.com>
2022-08-20 13:58:43 -0400

    Added main components for app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () =>  (
-    <div>App</div>
+    <div>
+        <CssBaseline />
+        <main>
+            <Switch>
+                <Route exact path='/movie/:id'>
+                    <h1>Movie information</h1>
+                </Route>
+                <Route exact path='/actors/:id'>
+                    <h1>Actors</h1>
+                </Route>
+                <Route exact path='/'>
+                    <h1>Movies</h1>
+                </Route>
+                <Route path='/profile/:id'>
+                    <h1>Profile</h1>
+                </Route>
+            </Switch>
+        </main>
+    </div>
 );
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import { BrowseGalleryTwoTone } from '@mui/icons-material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'),
+);
