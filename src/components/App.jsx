4266f34d4348b4253c8c5429db9461faaf1dc453
Tevin_Davis <tdavis.070991@gmail.com>
2022-08-20 14:16:55 -0400

    Added styles to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,38 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
-const App = () =>  (
-    <div>
-        <CssBaseline />
-        <main>
-            <Switch>
-                <Route exact path='/movie/:id'>
-                    <h1>Movie information</h1>
-                </Route>
-                <Route exact path='/actors/:id'>
-                    <h1>Actors</h1>
-                </Route>
-                <Route exact path='/'>
-                    <h1>Movies</h1>
-                </Route>
-                <Route path='/profile/:id'>
-                    <h1>Profile</h1>
-                </Route>
-            </Switch>
-        </main>
-    </div>
-);
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Navbar, Profile, Movies } from './';
+
+
+const App = () =>  {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Navbar />
+            <main className={classes.content}>
+            <div className={classes.toolbar} />
+                <Switch>
+                    <Route exact path='/movie/:id'>
+                        <MovieInformation />
+                    </Route>
+                    <Route exact path='/actors/:id'>
+                        <Actors />
+                    </Route>
+                    <Route exact path='/'>
+                        <Movies />
+                    </Route>
+                    <Route path='/profile/:id'>
+                        <Profile />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    )
+};
 
 
 export default App;
