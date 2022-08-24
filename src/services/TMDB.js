c4612bc43094f96a72eb068a5596a247839a53ec
tevin davis <aod.td1105@gmail.com>
2022-08-24 07:31:49 -0400

    setting up redux and calling from movie db

---
+++ src/services/TMDB.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.TMDB_KEY;
+const page = 1
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    //* Get movies by [Type]
+    getMovies: builder.query({
+      query: () => {
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+      }
+    })
+  }),
+})
