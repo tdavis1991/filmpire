33f970309c09c0715b5cd0df4f4b13b231b5083f
tevin davis <aod.td1105@gmail.com>
2022-09-01 05:31:40 -0400

    Implemented functioning search component

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
 
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
@@ -15,22 +14,24 @@ export const tmdbApi = createApi({
     }),
     //* Get movies by [Type]
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //* Get movies by search
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page${page}&api_key=${tmdbApiKey}`;
+        }
         //* Get movies by category
-        if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
           return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
         }
 
-        console.log(genreIdOrCategoryName)
-
         //* Get movies by genre
-        if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
           return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
         }
 
         //* Get popular movies by genre
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
-      }
+      },
     }),
   }),
 });
